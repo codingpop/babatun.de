dd73fbc38c24fd41598e880e331a2bfd48722106
Babatunde Adeyemi <tundewrites@gmail.com>
2020-11-24 17:13:16 +0100

    Implement blog post

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -11,8 +11,20 @@ interface NavLinkProps {
 const NavLink: FC<NavLinkProps> = ({ href, children, passHref = false }) => {
   const router = useRouter();
 
+  const isActive = () => {
+    if (router.pathname === href) {
+      return true;
+    }
+
+    if (router.pathname !== href && router.pathname.includes(href)) {
+      return true;
+    }
+
+    return false;
+  };
+
   let { className = '' } = children.props;
-  if (router.pathname === href) {
+  if (isActive()) {
     className = `${className} ${styles.selected}`;
   }
 
